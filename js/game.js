22b00cbd986e21c8677c6fe09225f336abdd1b33
User <sergioaotero26@hotmail.com>
2022-08-10 22:32:18 -0500

    Añadido boton resetear perfiles

--- js/game.js
+++ js/game.js
@@ -1,17 +1,17 @@
 availableCards = {
-    "A": { "value" : [1, 11], "available": 4, "availableURL": ["../img/cards/A_Corazon", "../img/cards/A_Diamante", "../img/cards/A_Picas", "../img/cards/A_Trebol"] },
-    "2": { "value" : [2], "available": 4, "availableURL": ["../img/cards/2_Corazon", "../img/cards/2_Diamante", "../img/cards/2_Picas", "../img/cards/2_Trebol"] },
-    "3": { "value" : [3], "available": 4, "availableURL": ["../img/cards/3_Corazon", "../img/cards/3_Diamante", "../img/cards/3_Picas", "../img/cards/3_Trebol"] },
-    "4": { "value" : [4], "available": 4, "availableURL": ["../img/cards/4_Corazon", "../img/cards/4_Diamante", "../img/cards/4_Picas", "../img/cards/4_Trebol"] },
-    "5": { "value" : [5], "available": 4, "availableURL": ["../img/cards/5_Corazon", "../img/cards/5_Diamante", "../img/cards/5_Picas", "../img/cards/5_Trebol"] },
-    "6": { "value" : [6], "available": 4, "availableURL": ["../img/cards/6_Corazon", "../img/cards/6_Diamante", "../img/cards/6_Picas", "../img/cards/6_Trebol"] },
-    "7": { "value" : [7], "available": 4, "availableURL": ["../img/cards/7_Corazon", "../img/cards/7_Diamante", "../img/cards/7_Picas", "../img/cards/7_Trebol"] },
-    "8": { "value" : [8], "available": 4, "availableURL": ["../img/cards/8_Corazon", "../img/cards/8_Diamante", "../img/cards/8_Picas", "../img/cards/8_Trebol"] },
-    "9": { "value" : [9], "available": 4, "availableURL": ["../img/cards/9_Corazon", "../img/cards/9_Diamante", "../img/cards/9_Picas", "../img/cards/9_Trebol"] },
-    "10":{ "value" : [10], "available": 4, "availableURL": ["../img/cards/10_Corazon", "../img/cards/10_Diamante", "../img/cards/10_Picas", "../img/cards/10_Trebol"] },
-    "J": { "value" : [10], "available": 4, "availableURL": ["../img/cards/J_Corazon", "../img/cards/J_Diamante", "../img/cards/J_Picas", "../img/cards/J_Trebol"] },
-    "Q": { "value" : [10], "available": 4, "availableURL": ["../img/cards/Q_Corazon", "../img/cards/Q_Diamante", "../img/cards/Q_Picas", "../img/cards/Q_Trebol"] },
-    "K": { "value" : [10], "available": 4, "availableURL": ["../img/cards/K_Corazon", "../img/cards/K_Diamante", "../img/cards/K_Picas", "../img/cards/K_Trebol"] },
+    "A": { "value" : [1, 11], "available": 4, "availableURL": ["../img/cards/png/A_Corazon.png", "../img/cards/png/A_Diamante.png", "../img/cards/png/A_Picas.png", "../img/cards/png/A_Trebol.png"] },
+    "2": { "value" : [2], "available": 4, "availableURL": ["../img/cards/png/2_Corazon.png", "../img/cards/png/2_Diamante.png", "../img/cards/png/2_Picas.png", "../img/cards/png/2_Trebol.png"] },
+    "3": { "value" : [3], "available": 4, "availableURL": ["../img/cards/png/3_Corazon.png", "../img/cards/png/3_Diamante.png", "../img/cards/png/3_Picas.png", "../img/cards/png/3_Trebol.png"] },
+    "4": { "value" : [4], "available": 4, "availableURL": ["../img/cards/png/4_Corazon.png", "../img/cards/png/4_Diamante.png", "../img/cards/png/4_Picas.png", "../img/cards/png/4_Trebol.png"] },
+    "5": { "value" : [5], "available": 4, "availableURL": ["../img/cards/png/5_Corazon.png", "../img/cards/png/5_Diamante.png", "../img/cards/png/5_Picas.png", "../img/cards/png/5_Trebol.png"] },
+    "6": { "value" : [6], "available": 4, "availableURL": ["../img/cards/png/6_Corazon.png", "../img/cards/png/6_Diamante.png", "../img/cards/png/6_Picas.png", "../img/cards/png/6_Trebol.png"] },
+    "7": { "value" : [7], "available": 4, "availableURL": ["../img/cards/png/7_Corazon.png", "../img/cards/png/7_Diamante.png", "../img/cards/png/7_Picas.png", "../img/cards/png/7_Trebol.png"] },
+    "8": { "value" : [8], "available": 4, "availableURL": ["../img/cards/png/8_Corazon.png", "../img/cards/png/8_Diamante.png", "../img/cards/png/8_Picas.png", "../img/cards/png/8_Trebol.png"] },
+    "9": { "value" : [9], "available": 4, "availableURL": ["../img/cards/png/9_Corazon.png", "../img/cards/png/9_Diamante.png", "../img/cards/png/9_Picas.png", "../img/cards/png/9_Trebol.png"] },
+    "10":{ "value" : [10], "available": 4, "availableURL": ["../img/cards/png/10_Corazon.png", "../img/cards/png/10_Diamante.png", "img/cards/png/10_Picas.png", "../img/cards/png/10_Trebol.png"] },
+    "J": { "value" : [10], "available": 4, "availableURL": ["../img/cards/png/J_Corazon.png", "../img/cards/png/J_Diamante.png", "../img/cards/png/J_Picas.png", "../img/cards/png/J_Trebol.png"] },
+    "Q": { "value" : [10], "available": 4, "availableURL": ["../img/cards/png/Q_Corazon.png", "../img/cards/png/Q_Diamante.png", "../img/cards/png/Q_Picas.png", "../img/cards/png/Q_Trebol.png"] },
+    "K": { "value" : [10], "available": 4, "availableURL": ["../img/cards/png/K_Corazon.png", "../img/cards/png/K_Diamante.png", "../img/cards/png/K_Picas.png", "../img/cards/png/K_Trebol.png"] },
 }
 
 var init = function () {
@@ -24,19 +24,37 @@ var init = function () {
     this.money = actualProfile.money;
     this.cardsInTable = ko.observableArray();
 
+    const PKR = value => currency(value, { precision: 2, symbol: '♠' });
+
+    changeToCurrency = function(){
+        if(this.money > 999 && money < 1000000){
+            return PKR(this.money/1000).format() + 'K'; // convert money into Thousands
+        }else if(this.money >= 1000000){
+            return PKR(this.money/1000000).format() + 'M'; // convert money into Millions
+        }else if(this.money >= 1000000000){
+            return PKR(this.money/1000000000).format() + 'B'; // convert money into Billions
+        }else if(this.money >= 1000000000000){
+            return PKR(this.money/1000000000000).format() + 'T'; // convert money into Trillions
+        }
+        return PKR(this.money).format();
+    }
+
     prueba = function(){
         console.log(availableCardsKeys);
     }
 
     añadirCarta = function(){
+        //Saca un tipo de carta al azar
         newCard = availableCardsKeys[Math.floor(Math.random()*availableCardsKeys.length)];
-        console.log( "carta" + newCard + availableCards[newCard]["available"]);
+        //verifica que todavia queden cartas de ese tipo
         if (availableCards[newCard]["available"] == 1){
-            console.log("antes" + availableCardsKeys);
             availableCardsKeys.splice(availableCardsKeys.indexOf(newCard), 1);
-            console.log("despues" + availableCardsKeys);
         }
         availableCards[newCard]["available"] -= 1;
+        //Saca una carta del tipo seleccionado al azar
+        newCardUrl = availableCards[newCard]["availableURL"][Math.floor(Math.random()*availableCards[newCard]["availableURL"].length)]
+        this.cardsInTable.push({card : newCard, cardSrc : newCardUrl});
+        console.log(cardsInTable)
     }
 }
 
