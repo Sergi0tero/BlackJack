bb61e57fd95d32bfd31b9fdf148319a09568a6df
User <sergioaotero26@hotmail.com>
2022-08-15 14:44:07 -0500

    Botones juego

--- js/game.js
+++ js/game.js
@@ -14,36 +14,36 @@ availableCards = {
     "K": { "value" : [10], "available": 4, "availableURL": ["../img/cards/png/K_Corazon.png", "../img/cards/png/K_Diamante.png", "../img/cards/png/K_Picas.png", "../img/cards/png/K_Trebol.png"] },
 }
 
-var init = function () {
+var game = function () {
+
     localStorage.availableCards = JSON.stringify(availableCards);
     actualProfile = JSON.parse(localStorage.actualProfile);
 
     this.availableCardsKeys = Object.keys(availableCards);
     this.profileName = actualProfile.name;
     this.imgSrc = actualProfile.imgSrc;
-    this.money = actualProfile.money;
-    this.cardsInTable = ko.observableArray();
+    
+    this.playerCards = ko.observableArray();
+    this.dealerCards = ko.observableArray();
+    this.observableMoney = ko.observable(actualProfile.money);
+    this.betInTable = ko.observable(0);
 
     const PKR = value => currency(value, { precision: 2, symbol: '♠' });
 
     changeToCurrency = function(){
-        if(this.money > 999 && money < 1000000){
-            return PKR(this.money/1000).format() + 'K'; // convert money into Thousands
-        }else if(this.money >= 1000000){
-            return PKR(this.money/1000000).format() + 'M'; // convert money into Millions
-        }else if(this.money >= 1000000000){
-            return PKR(this.money/1000000000).format() + 'B'; // convert money into Billions
-        }else if(this.money >= 1000000000000){
-            return PKR(this.money/1000000000000).format() + 'T'; // convert money into Trillions
+        if(observableMoney() > 999 && money < 1000000){
+            return PKR(observableMoney()/1000).format() + 'K'; // convert money into Thousands
+        }else if(observableMoney() >= 1000000){
+            return PKR(observableMoney()/1000000).format() + 'M'; // convert money into Millions
+        }else if(observableMoney() >= 1000000000){
+            return PKR(observableMoney()/1000000000).format() + 'B'; // convert money into Billions
+        }else if(observableMoney() >= 1000000000000){
+            return PKR(observableMoney()/1000000000000).format() + 'T'; // convert money into Trillions
         }
-        return PKR(this.money).format();
+        return PKR(observableMoney()).format();
     }
 
-    prueba = function(){
-        console.log(availableCardsKeys);
-    }
-
-    añadirCarta = function(){
+    pedirCarta = function(){
         //Saca un tipo de carta al azar
         newCard = availableCardsKeys[Math.floor(Math.random()*availableCardsKeys.length)];
         //verifica que todavia queden cartas de ese tipo
@@ -55,9 +55,111 @@ var init = function () {
         urlIndex = Math.floor(Math.random()*availableCards[newCard]["availableURL"].length);
         newCardUrl = availableCards[newCard]["availableURL"][urlIndex];
         availableCards[newCard]["availableURL"].splice(urlIndex, 1);
-        this.cardsInTable.push({card : newCard, cardSrc : newCardUrl});
-        console.log(cardsInTable);
+        this.playerCards.push({card : newCard, cardSrc : newCardUrl});
+        //console.log(playerCards());
+    }
+
+    //Botones de apuestas
+    betTen = function(){
+        var moneyAmount = getMoneyAmount(10);
+        if ((this.observableMoney() - moneyAmount) >= 0){
+            document.querySelector(".iniciar").style.display = "inline-block";
+            document.querySelector(".cancelar").style.display = "inline-block";
+            this.betInTable(this.betInTable() + moneyAmount);
+            this.observableMoney(this.observableMoney() - moneyAmount);
+            console.log(this.betInTable());
+        }
+    }
+    betFifty = function(){
+        var moneyAmount = getMoneyAmount(50);
+        if ((this.observableMoney() - moneyAmount) >= 0){
+            document.querySelector(".iniciar").style.display = "inline-block";
+            document.querySelector(".cancelar").style.display = "inline-block";
+            this.betInTable(this.betInTable() + moneyAmount);
+            this.observableMoney(this.observableMoney() - moneyAmount);
+            console.log(this.betInTable());
+        }
+    }
+    betHundred = function(){
+        var moneyAmount = getMoneyAmount(100);
+        if ((this.observableMoney() - moneyAmount) >= 0){
+            document.querySelector(".iniciar").style.display = "inline-block";
+            document.querySelector(".cancelar").style.display = "inline-block";
+            this.betInTable(this.betInTable() + moneyAmount);
+            this.observableMoney(this.observableMoney() - moneyAmount);
+            console.log(this.betInTable());
+        }
+    }
+    betAllIn = function(){
+        if (this.observableMoney() > 0){
+            document.querySelector(".iniciar").style.display = "inline-block";
+            document.querySelector(".cancelar").style.display = "inline-block";
+            this.betInTable(this.betInTable() + this.observableMoney());
+            this.observableMoney(this.observableMoney() - this.observableMoney());
+            console.log(this.betInTable());
+        }
+    }
+
+    //Controles del juego
+    confirmarApuesta = function(){
+        document.querySelector(".pedir").style.display = "inline-block";
+        document.querySelector(".terminar").style.display = "inline-block";
+        document.querySelector(".iniciar").style.display = "none";
+        document.querySelector(".cancelar").style.display = "none";
+        console.log("confirma");
+    }
+    cancelarApuesta = function(){
+        document.querySelector(".iniciar").style.display = "none";
+        document.querySelector(".cancelar").style.display = "none";
+        console.log("cancela");
+    }
+    pedirCarta = function(){
+        console.log("pide");
+    }
+    terminarJuego = function(){
+        console.log("termina");
+    }
+
+    //Gana o pierde el usuario
+
+    win = function(){
+        this.observableMoney(observableMoney() + (betInTable() * 2));
+        this.actualProfile.money = observableMoney();
+        localStorage.actualProfile = JSON.parse(this.actualProfile);
+    }
+
+    lose = function(){
+        this.observableMoney(observableMoney() - betInTable());
+        this.actualProfile.money = observableMoney();
+        localStorage.actualProfile = JSON.parse(this.actualProfile);
+    }
+
+    //Devuelve la cantidad de la apuesta dependiendo cuanto dinero tiene el usuario
+    getMoneyAmount = function(bet){
+        if(this.observableMoney() <= 100){
+            return bet / 10;
+        } else if(this.observableMoney() <= 1000){
+            return bet;
+        }else if( this.observableMoney() <= 10000){
+            return bet * 10;
+        }else if( this.observableMoney() <= 100000){
+            return bet * 100;
+        }else if( this.observableMoney() <= 1000000){
+            return bet * 1000;
+        }else if( this.observableMoney() <= 10000000){
+            return bet * 10000;
+        }else if( this.observableMoney() <= 100000000){
+            return bet * 100000;
+        }else if( this.observableMoney() <= 1000000000){
+            return bet * 1000000;
+        }else if( this.observableMoney() <= 10000000000){
+            return bet * 10000000;
+        }else if( this.observableMoney() <= 100000000000){
+            return bet * 100000000;
+        }else if( this.observableMoney() <= 1000000000000){
+            return bet * 1000000000;
+        }
     }
 }
 
-ko.applyBindings(init());
+ko.applyBindings(game());
